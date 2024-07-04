7bae89a6798fb6cc2d98e282708c1a6ded250911
djogeology <bzwajih@gmail.com>
2024-07-04 14:57:36 +0100

    shop backend functions tested

--- BackEnd/routes/shop.js
+++ BackEnd/routes/shop.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller=require("../controllers/shop")
 
->>>>>>> 869d9757c5c53e59cea59c39e9f456f47d55c49a
-
 router.get('/get', controller.getAllShop);
 router.get('/get/:id', controller.getShopById);
 router.get('/category/:category', controller.getByCategory);
