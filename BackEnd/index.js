8ff6c0f327a18f994a8d0ff039fe75fbf583d816
djogeology <bzwajih@gmail.com>
2024-07-04 13:35:13 +0100

    shop thunder and schema

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -12,7 +12,9 @@ app.use(cors());
 
 app.use(express.json({limit:'500mb'}));
 app.use(express.urlencoded({extended:true, limit:'500mb'}));
-app.use(express.static(__dirname + '/../client/dist'))
+app.use(express.static(__dirname + '/../client/dist'));
+app.use("/api/shop",shopRoute);
+app.use("/api/user",userRoute);
 
 
 app.listen(port, () => {
--- BackEnd/routes/shop.js
+++ BackEnd/routes/shop.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const controller=require("../controllers/shop")
 
 
-router.get('/', controller.getAllShop);
-router.get('/:id', controller.getShopById);
+router.get('/get', controller.getAllShop);
+router.get('/get/:id', controller.getShopById);
 router.get('/category/:category', controller.getByCategory);
-router.post('/', controller.addshop);
-router.put('/:id', controller.updateshop);
-router.delete('/:id', controller.deleteshop);
+router.post('/add', controller.addshop);
+router.put('/update/:id', controller.updateshop);
+router.delete('/delete/:id', controller.deleteshop);
 
 module.exports = router;
 
