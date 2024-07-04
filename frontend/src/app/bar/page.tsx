4acb9d62efb3efe6b0a2ca7c8ee0ccbc56bd0fb5
mehrez <mehrezhammami3@gmail.com>
2024-07-04 20:54:00 +0100

    d

--- frontend/src/app/bar/page.tsx
+++ frontend/src/app/bar/page.tsx
@@ -1,32 +1,32 @@
 // src/app/bar/page.tsx
-
 'use client'; // Ensure client-side rendering
 
 import React from 'react';
 import { useShops, Shop } from '../../contexts/shopsContext';
 import Link from 'next/link'; // Import Link from Next.js
+import styles from './bar.module.css'; // Import the styles
 
 const BarPage: React.FC = () => {
   const { shops } = useShops();
   const barShops = shops.filter(shop => shop.category === 'bar');
 
   return (
-    <div>
+    <div className={styles.coffeeContainer}>
       <h2>Bars</h2>
-      <ul>
+      <div className={styles.coffeeList}>
         {barShops.map((shop: Shop) => (
-          <li key={shop.id}>
-            <img src={shop.image} alt={shop.name} />
-            <div>
+          <div key={shop.id} className={styles.coffeeCard}>
+            <img src={shop.logo} alt={shop.name} className={styles.coffeeImage} />
+            <div className={styles.coffeeDetails}>
               <h3>{shop.name}</h3>
               <p>{shop.address}</p>
               <Link href={`/bar/${shop.id}`}>
-                <a>View Details</a>
+                <span className={styles.detailsButton}>View Details</span>
               </Link>
             </div>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
