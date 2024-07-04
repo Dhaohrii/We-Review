5e1577d9187fabb6c111fd5ca1ac225b953f266c
djogeology <bzwajih@gmail.com>
2024-07-04 12:23:54 +0100

    s

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,9 +5,10 @@ const port = 5000;
 const db =require("./Database/db")
 
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json({limit:'500mb'}));
+app.use(express.urlencoded({limit:'500mb',extended:true}));
 
 
 app.listen(port, () => {
