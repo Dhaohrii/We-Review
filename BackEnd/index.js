c0f9c69ce7f6fa5f8b3bc06aae6a079448f22e22
Dhaohrii <dhaohrii@yahoo.com>
2024-07-06 10:17:21 -0300

    registerAlmostDone, BackEndLogin almostDone with jwt & bcrypt

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,13 +5,15 @@ const port = 5000;
 const db =require("./Database/db")
 const bodyParser=require("body-parser")
 const userRoute=require("./routes/user")
-const shopRoute=require('./routes/shop')
+const shopRoute=require('./routes/shop');
+const cookieParser = require("cookie-parser");
 
 
 app.use(cors())
 
 app.use(express.json())
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use('/api/user',userRoute);
 app.use('/api/shop',shopRoute)
 
