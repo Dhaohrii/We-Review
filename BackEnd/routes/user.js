cc45cfc26a374fb219e938e0a814f890ac6a57c6
Dhaohrii <dhaohrii@yahoo.com>
2024-07-07 02:11:53 -0300

    login done, fixing some issues in shop by categorie and by id

--- BackEnd/routes/user.js
+++ BackEnd/routes/user.js
@@ -1,8 +1,12 @@
 const express=require('express');
 const route=express.Router();
 const { createUser, checkLogin } = require('../controllers/user');
+const userCheck=require("../controllers/LogonChecker")
 
 route.post('/add',createUser);
 route.post('/login',checkLogin)
+route.get("/isloged",userCheck,(req,res)=>{
+    res.status(200).json({user:req.user})
+  });
 
 module.exports = route;
